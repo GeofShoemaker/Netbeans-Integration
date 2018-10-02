9f4a5c70e600a5cb0cf05032b6cb385cd4bd7a10
Geof Shoemaker <shoemakergeofrey20@greenbrierschools.org>
2018-10-01 19:11:11 -0500

    Working on Third.java

--- src/Third.java
+++ src/Third.java
@@ -1,40 +1,34 @@
+
 import java.util.*;
+
 public class Third {
+
     public static void main(String[] args) {
-        ArrayList<String> nums = new ArrayList<>();
-        //Max int: 2147483647
-        //Min int: -2147483648
-        Scanner r = new Scanner(System.in);
-        String in = r.nextLine();
-        String[] n = in.split(" ");
-        for(String o:n)
-            nums.add(o);
-        System.out.println(calculate(nums));
-        
-
-        // 10 5 + 2 3 - - 2 *
+        Scanner reader = new Scanner(System.in);
+        String e =reader.nextLine();
+        System.out.println(eval(e));
     }
-    
-    static int calculate(ArrayList<String> arr) {
-        
-        while(arr.size()>1){
-            int x = Integer.parseInt(arr.get(0));
-            int y = Integer.parseInt(arr.get(1));
-            String op = arr.get(2);
-            if(op.equals("*"))
-                x*=y;
-            if(op.equals("+"))
-                x+=y;
-            if(op.equals("-"))
-                x-=y;
-            arr.set(0,Integer.toString(x));
-            arr.remove(1);
-            arr.remove(2);
-            System.out.println(arr);
+    static int eval(String eq) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        Scanner r = new Scanner(eq);
+
+        while (r.hasNext()) {
+            if (r.hasNextInt()) 
+                nums.add(r.nextInt());
+             else {
+                String operator = r.next();
+                int x = nums.remove(nums.size()-2);
+                int y = nums.remove(nums.size()-1);
+                if (operator.equals("*")) 
+                    nums.add(x * y);
+                 else if (operator.equals("+")) 
+                    nums.add(x + y);
+                 else if (operator.equals("-")) 
+                    nums.add(x - y);
+                 else 
+                    nums.add(x / y);
+            }
         }
-        
-       return 0;
-
+        return nums.remove(0);
     }
-    
 }
